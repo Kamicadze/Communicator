f6b0de27d8bde7b986f4debd2a4349493faf55e2
Kamicadze <Kamicadze1@gmail.com>
2016-09-09 11:32:05 +0100

    finished work queue

--- Server/src/CWorkQueue.cpp
+++ Server/src/CWorkQueue.cpp
@@ -20,6 +20,61 @@ CWorkQueue::~CWorkQueue()
 	pthread_cond_destroy(&wcond);
 }
 
-ITask* CWorkQueue::nextTask()
+ITask* CWorkQueue::m_nextTask()
 {
 	ITask *o_nt=0;
+
+	//locking the mutex
+	pthread_mutex_lock(&qmtx);
+	//looking for work
+	if(finished && tasks.size()==0);
+	{
+		o_nt=0;
+	}
+	else
+	{
+		if(tasks.size()==0)
+		{
+			pthread_cond_wait(&wcond, &qmtx);
+		}
+		o_nt=tasks.front();
+		tasks.pop();
+
+		//for debbuging
+		//if(nt) nt->showTask();
+	}
+
+	//unlock mutex
+	pthread_mutex_unlock(&qmtx);
+	return o_nt;
+}
+
+void CWorkQueue::m_addTask(Task *o_nt)
+{
+	if(!finished)
+	{
+		//lock queue
+		pthread_mutex_lock(&qmtx);
+		tasks.push(o_nt);
+		//signaling the new work
+		pthread_cond_signal(&wcond);
+		//unlock mutex
+		pthread_mutex_unlock(&qmtx);
+	}
+}
+
+//setting the queue to finished
+void CWorkQueue::m_finished()
+{
+	pthread_mutex_lock(&qmtx);
+	finished=true;
+	pthread_cond_signal(&wcond);
+	pthread_mutex_unlock(&qmtx);
+}
+
+bool CWorkQueue::m_hasWork()
+{
+	return(tasks.size()>0);
+}
+
+
